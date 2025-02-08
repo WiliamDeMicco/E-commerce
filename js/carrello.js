aff7efc3502e52acd3c95d8d4d95faeb3c836c00
Giusy-Paccione <giusy_paccione@outlook.it>
2025-02-08 13:23:18 +0100

    aggiornamento quantità e prezzo js

--- js/carrello.js
+++ js/carrello.js
@@ -4,29 +4,38 @@
 const plus = document.querySelector(".plus"),
     minus = document.querySelector(".minus"),
     num = document.querySelector(".num");
+price = document.getElementById("price");
+total = document.querySelector(".riepilogo p strong");
+remove = document.querySelector(".rimuovi");
 
-let a = 1;
+let qta = 1;
+let unitPrice = parseFloat(price.innerText.replace("€", "").replace(",", "."));
+
 plus.addEventListener("click", () => {
 
-    if (a < 10) {
-        a++;
+    if (qta < 10) {
+        qta++;
 
-        num.innerText = a;
-        console.log(a);
+        num.innerText = qta;
+        updateTotPrice();
+        console.log(qta);
     }
 });
 
 minus.addEventListener("click", () => {
-    if (a > 1) {
-        a--;
+    if (qta > 1) {
+        qta--;
 
-        num.innerText = a;
-        console.log(a);
+        num.innerText = qta;
+        updateTotPrice();
+        console.log(qta);
     }
 });
 
-
-//aggiorna prezzo in base alla quantità
-
+function updateTotPrice() {
+    let newTot = (qta * unitPrice).toFixed(2).replace(".", ",");
+    price.innerText = `€${newTot}`;
+    total.innerText = `€${newTot}`;
+}
 
 
