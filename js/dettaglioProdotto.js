c7e47805664929a75c3ee32d7011486ca43d1da7
Luca Di Giulio <lkdgl95@gmail.com>
2025-02-17 10:29:59 +0100

    aggiornamento dettaglio_prodotto

--- js/dettaglioProdotto.js
+++ js/dettaglioProdotto.js
@@ -1,16 +1,16 @@
 function getQueryParam(param) {
-    const params = new URLSearchParams(window.location.search);
-    return params.get(param);
+  const params = new URLSearchParams(window.location.search);
+  return params.get(param);
 }
 
 const productId = getQueryParam('id');
 
 
 fetch('https://fakestoreapi.com/products/' + productId)
-    .then(response => response.json())
-    .then(product => {
-        const container = document.getElementById('dettaglio');     // punto l'elemento dove voglio inserire quello che segue
-        container.innerHTML = `
+  .then(response => response.json())
+  .then(product => {
+    const container = document.getElementById('dettaglio');     // punto l'elemento dove voglio inserire quello che segue
+    container.innerHTML = `
             <div class="row">
                 <div class="img col-3 mr-1"> 
                     <img src="${product.image}" class="card-img-top" alt="${product.title}">
@@ -30,44 +30,47 @@ fetch('https://fakestoreapi.com/products/' + productId)
                     </div>
                     <br><br>
                     <a href="carrello.html" class="btn">Aggiungi al carrello</a><br>
-                    <a href="abbigliamento.html" class="btn btn-primary bottoneDettaglio">← Torna alla pagina prodotti</a>
+                    <a href="abbigliamento.html" class="btn btn-primary bottoneDettaglio mb-4">← Torna alla pagina prodotti</a>
                 </div>
             </div>`;
+  })
+  .catch(error => console.error('Errore nel recupero del prodotto:', error));
+/*
+    const container = document.getElementById('articolo');
+    container.innerHTML=`
+    <li>{product.description}</li>
+    `
+    const riepilogo = document.getElementById('lista_prezzi');
+    container.innerHTML=`
+    <li>{product.price}</li>
+    `
+
+    const riepilogo = document.getElementById('lista_riepilogo');
+    riepilogo.innerHTML=`
+    <li>x1 {product.title} = {product.price}</li>
+    `
+*/
+
+
+const carosello = document.getElementById('carosello');
+
+fetch('https://fakestoreapi.com/products?limit=3')
+    .then(response => response.json())
+    .then(products => {
+        const productsCards = products.map(product => {
+         
+            return `
+        <div class="card m-2 consigliati d-flex flex-column justify-content-between">
+        <div>
+        <img src="${product.image}" alt="${product.title}">
+        </div>
+        <div class="card-body d-flex  flex-column justify-content-between">
+          <h5 class="card-title">${product.title}</h5>
+          <a href="dettaglioProdotto.html?id=${product.id}" class="btn btn-primary discover_more">Scopri di più</a>
+        </div>
+      </div>
+    `;
+        }).join('');
+        carosello.innerHTML = productsCards;
     })
-    .catch(error => console.error('Errore nel recupero del prodotto:', error));
-
-    
-     fetch('https://fakestoreapi.com/products')
-        .then(response => response.json())
-        .then(product => {
-            const Carosello = document.getElementById('Carosello');
-            Carosello.innerHTML = `
-           
-                <div class="item">
-                    <img src="${product.title}" alt="${product.title}"> 
-                    <h4>${product.title}</h4>
-                    <br>
-                    <h1>€${product.price}</h1>
-                </div>
-                `;
-                console.log("chiamata effettuata con successo");
-        })
-      .catch(error => console.error('Errore nel recupero del prodotto:', error));
-
-    /* 
-        const container = document.getElementById('articolo');
-        container.innerHTML=`
-        <li>{product.description}</li>
-        `
-        const riepilogo = document.getElementById('lista_prezzi');
-        container.innerHTML=`
-        <li>{product.price}</li>
-        `
-
-        const riepilogo = document.getElementById('lista_riepilogo');
-        riepilogo.innerHTML=`
-        <li>x1 {product.title} = {product.price}</li>
-        `
-    */
-        
-        
+    .catch(error => console.error('Errore nel recupero dei prodotti:', error));
