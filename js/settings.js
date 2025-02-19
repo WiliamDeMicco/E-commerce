7f9dc68fdbc74ec2869f2ebe5d636716b0d1bcea
Alessandro Marasca <linkmarasca@gmail.com>
2025-02-19 12:22:53 +0100

    settings js complete e settings html (form id cambiati)

--- js/settings.js
+++ js/settings.js
@@ -1,3 +1,51 @@
+// Funzione per effettuare il logout
+function logout() {
+    // Recupera il token dal localStorage
+    const token = localStorage.getItem("authToken");        // DA PRENDERE PER L'AGGIORNAMENTO
+    if (!token) {
+        console.error("Nessun token trovato nel localStorage");
+        return;
+    }
+
+    fetch('http://localhost:8080/api/users/' + token, {                // CAMBIARE ENDPOINT ('http://localhost:8080/api/users/'+token)
+        method: 'PUT',                                        // method: 'PUT'
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+        }
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Modifica fallita');             // modifica fallita
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Modifica effettuata:', data);            // modifica effettuata
+            // Rimuove il token dal localStorage
+            // Nasconde il pulsante Logout e mostra il form di login
+            /*document.getElementById("logoutButton").style.display = "none";
+            document.getElementById("loginForm").style.display = "block";*/
+        })
+        .catch(error => {
+            console.error('Errore durante il logout:', error);
+            printOutput({ error: error.message });
+        });
+}
+document.getElementById('settings').addEventListener('submit', function (event) {             // da prendere integralmente
+    event.preventDefault();
+    const newUser = {                       // const aggiornamento
+        name: document.getElementById('fName').value,
+        email: document.getElementById('typeEmailX').value,
+        cognome: document.getElementById('lName').value,
+        partitaIva: document.getElementById('pIva').value,
+        password: document.getElementById('creapw').value
+    };
+    addUser(newUser);                       // update(aggiornamento)
+});
+
+
+
 document.addEventListener("DOMContentLoaded", function () {
     const form = document.querySelector(".settings");
     const button = document.querySelector(".btn.bg-warning");
@@ -10,4 +58,4 @@ document.addEventListener("DOMContentLoaded", function () {
         cardNome.textContent = nome || "Nome";
         cardCognome.textContent = cognome || "Cognome";
     });
-});
+})
